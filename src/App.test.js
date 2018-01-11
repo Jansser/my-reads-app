54de7b3145475fa54de465a501deff9a4ec6552c
Jansser <jansser_costa@hotmail.com>
2018-01-11 09:29:33 -0300

    test:AppTest

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { MemoryRouter } from 'react-router';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import BookShelves from './components/BookShelves';
+
+
+const BooksAPI = {
+  getAll: jest.fn().mockImplementation( getAll => [] ) 
+}
 
 it('renders without crashing', () => {
-  shallow(<MemoryRouter><App /></MemoryRouter>);
+  expect(shallow(<MemoryRouter><App /></MemoryRouter>));
+});
+
+it('renders <BookShelves />', () => {
+  BooksAPI.getAll.mockImplementation(getAll => {[]});
+
+  const wrapper = mount(<MemoryRouter><App /></MemoryRouter>);
+  
+  expect(wrapper.find(BookShelves).length).toBe(1);
 });
